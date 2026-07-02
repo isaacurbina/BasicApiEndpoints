64bdbee7cb93c3f65ae102dc55dde687459b97d7
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2026-07-01 19:08:19 -0500

    API Endpoints: Implementation Guide (#2)

--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Register MVC controller services
+builder.Services.AddControllers();
+
 var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+// Ensure endpoints are mapped after AddControllers
+app.MapControllers();
 
 app.MapGet("/", () => "Root Path");
 app.MapGet("/downloads", () => "Downloads");
